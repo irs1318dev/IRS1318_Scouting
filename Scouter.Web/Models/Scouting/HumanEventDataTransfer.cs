12f70dedb493a9d7b0e42c9cd5fa06ad91866b33
Chris Knue <C.knue@digipen.edu>
2015-03-09 14:54:55 -0700

    Added HumanEventDataTransfer

---
+++ Scouter.Web/Models/Scouting/HumanEventDataTransfer.cs
@@ -0,0 +1,19 @@
+﻿using Scouter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scouter.Web.Models.Scouting
+{
+    public class HumanEventDataTransfer
+    {
+        public int Id { get; set; }
+        public int Scouter_Id { get; set; }
+        /// <summary>
+        /// EG: (Red)1 (Blue)2 (insert color here)3
+        /// </summary>
+        public int Team_Number { get; set; }
+        public HumanEventType HumanEventType { get; set; }
+    }
+}
