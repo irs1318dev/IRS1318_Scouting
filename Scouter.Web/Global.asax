fe7b93739d483750793238911a56f885fccd264e
Neko81795 <Neko81795@yahoo.com>
2015-10-08 19:09:49 -0700

    Reset the entire project

--- Scouter.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Scouter.Web.MvcApplication" Language="C#" %>
--- Scouter.Web/ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Scouter.Models;
-
-namespace Scouter.Web.ViewModels
-{
-    public class ViewModelBase
-    {
-        // implement CanEdit, CanDelete here if wanted
-    }
-}
