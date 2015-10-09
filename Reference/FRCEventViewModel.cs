79105dbf1c0f9fa8cd1dac02f218d70f494b9acc
Neko81795 <Neko81795@yahoo.com>
2015-10-08 20:05:32 -0700

    Still broken

---
+++ Reference/FRCEventViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Scouter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scouter.Web.ViewModels
+{
+    public class FRCEventViewModel
+    {
+        public FRCCompetition Event { get; set; }
+        public bool IsNew { get; set; }
+
+        public FRCEventViewModel()
+        {
+            this.Event = new FRCCompetition();
+        }
+    }
+}
