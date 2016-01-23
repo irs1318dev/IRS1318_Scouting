6f2ecc4a1ceb926e4a0ea355435555eccb9e8eed
magital15 <talimagidson@hotmail.com>
2016-01-23 16:42:31 -0600

    Started loading XML

--- Scouting Server/Scouting Server/Form1.cs
+++ Scouting Server/Scouting Server/Form1.cs
@@ -8,51 +8,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Scouting_Server
 {
-  public partial class Form1 : Form
-  {
-    Data.DataFile<Models.Test> test;
-    Net.NetworkServer serv;
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        Data.DataFile<Models.Test> test;
+        Net.NetworkServer serv;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
 
-      string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.csv";
-      test = new Data.DataFile<Models.Test>(path);
-      Models.Test testData = new Models.Test();
-      testData.MyProperty = 1000;
-      testData.TheWortzmanOne = 2000;
-      test.Add(testData);
-      test.Add(new Models.Test() { MyProperty = 1, what = 2, theThird = 3, myAction = 4, TheWortzmanOne = 5 });
-      ulong idVal = test.Add(new Models.Test() { MyProperty = 5, what = 4, theThird = 3, myAction = 2, TheWortzmanOne = 99 });
-      test.Remove(idVal);
-      test.Add(new Models.Test() { MyProperty = 5, what = 4, theThird = 3, myAction = 2, TheWortzmanOne = 99 });
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.csv";
+            //test = new Data.DataFile<Models.Test>(path);
+            //Models.Test testData = new Models.Test();
+            //testData.MyProperty = 1000;
+            //testData.TheWortzmanOne = 2000;
+            //test.Add(testData);
+            //test.Add(new Models.Test() { MyProperty = 1, what = 2, theThird = 3, myAction = 4, TheWortzmanOne = 5 });
+            //ulong idVal = test.Add(new Models.Test() { MyProperty = 5, what = 4, theThird = 3, myAction = 2, TheWortzmanOne = 99 });
+            //test.Remove(idVal);
+            //test.Add(new Models.Test() { MyProperty = 5, what = 4, theThird = 3, myAction = 2, TheWortzmanOne = 99 });
 
-      if (test.Exists(10))
-      {
-        var model = test.Get(10);
-        model.myAction = 9999999;
-        test.Update(model);
-      }
 
-      test.Save();
+            //if (test.Exists(10))
+            //{
+            //  var model = test.Get(10);
+            //  model.myAction = 9999999;
+            //  test.Update(model);
+            //}
 
-      //network
-      serv = new Net.NetworkServer(444);
-      serv.Connected += Serv_Connected;
-      serv.DataAvailable += Serv_DataAvailable;
+            //test.Save();
+
+            ////network
+            //serv = new Net.NetworkServer(444);
+            //serv.Connected += Serv_Connected;
+            //serv.DataAvailable += Serv_DataAvailable;
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("path");
+            XmlElement v = doc.DocumentElement;
+            XmlNodeList autolist = v.GetElementsByTagName("Auto");
+            XmlNode autonnode = autolist[0];
+            XmlElement offence = autonnode["Offence"];
+            XmlNodeList actions = offence.GetElementsByTagName("Action");
+            loadObjects(actions, "Auto", "Offense");
+        }
+        public void loadObjects(XmlNodeList actions, String pageName, String catagoryName)
+        {
+            String[] objectName = new String[actions.Count];
+            int[] objectType = new int[actions.Count];
+            if (pageName != null)
+            {
+                objectName[i] = pageName;
+                i++;
+            }
+            objectName[i] = catagoryName;
+            objectType[i] = 2;
+            for (i++; i < actions.Count;)
+            {
+                objectName[i] = actions[i].Attributes["Name"].ToString();
+                switch (actions[i].Attributes["Type"].ToString())
+                {
+                    case "Check":
+                        objectType[i] = 3;
+                        break;
+                    case "Counter":
+                        objectType[i] = 4;
+                        break;
+                    case "Slider":
+                        objectType[i] = 5;
+                        break;
+                    case "Choice":
+                        objectType[i] = 6;
+                        break;
+                    case "Text":
+                        objectType[i] = 8;
+                        break;
+                }
+            }
     }
-
     private void Serv_DataAvailable(object sender)
     {
 
     }
-
     private void Serv_Connected(object sender)
     {
 
     }
-  }
+}
 }
