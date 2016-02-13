f01be051aeb1aec325bf4d8eedb357879ca22df4
magital15 <talimagidson@hotmail.com>
2016-02-13 13:41:44 -0600

    Adjusted XML reading

--- Scouting Server/Scouting Server/Form1.Designer.cs
+++ Scouting Server/Scouting Server/Form1.Designer.cs
@@ -28,34 +28,46 @@
     /// </summary>
     private void InitializeComponent()
     {
-            this.button1 = new System.Windows.Forms.Button();
+            this.objectsText = new System.Windows.Forms.Label();
+            this.devicesText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // objectsText
             // 
-            this.button1.Location = new System.Drawing.Point(337, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.objectsText.AutoSize = true;
+            this.objectsText.Location = new System.Drawing.Point(344, 195);
+            this.objectsText.Name = "objectsText";
+            this.objectsText.Size = new System.Drawing.Size(43, 13);
+            this.objectsText.TabIndex = 0;
+            this.objectsText.Text = "Objects";
+            // 
+            // devicesText
+            // 
+            this.devicesText.AutoSize = true;
+            this.devicesText.Location = new System.Drawing.Point(347, 224);
+            this.devicesText.Name = "devicesText";
+            this.devicesText.Size = new System.Drawing.Size(46, 13);
+            this.devicesText.TabIndex = 1;
+            this.devicesText.Text = "Devices";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1121, 472);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.devicesText);
+            this.Controls.Add(this.objectsText);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Scouter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label objectsText;
+        private System.Windows.Forms.Label devicesText;
     }
 }
 
--- Scouting Server/Scouting Server/Form1.cs
+++ Scouting Server/Scouting Server/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,11 @@ namespace Scouting_Server
 {
     public partial class Form1 : Form
     {
+        int devices = 0;
         Data.DataFile<Models.Test> test;
         Net.NetworkServer serv;
-        int i = 0;
+        List<String> objectName = new List<String>();
+        List<int> objectType = new List<int>();
         public Form1()
         {
             InitializeComponent();
@@ -49,57 +52,74 @@ namespace Scouting_Server
 
 
             XmlDocument doc = new XmlDocument();
-            doc.Load("path");
+            doc.Load("path.xml");
+            string pageName = "";
             XmlElement v = doc.DocumentElement;
-            XmlNodeList mainlist = v.GetElementsByTagName("Auto");
-            XmlNode mainnode = mainlist[0];
-            XmlElement catagory = mainnode["Offence"];
-            loadObjects(catagory, "Auto", "Offense");
-            catagory = mainnode["Defence"];
-            loadObjects(catagory, null, "Defence");
-            mainlist = v.GetElementsByTagName("Teleop");
-            mainnode = mainlist[0];
-            catagory = mainnode["Offense"];
-            loadObjects(catagory, "Teleop", "Offense");
-            catagory = mainnode["Defence"];
-            loadObjects(catagory, null, "Defence"); 
-        }
-        public void loadObjects(XmlElement catagory, String pageName, String catagoryName)
-        {
-            XmlNodeList actions = catagory.GetElementsByTagName("Action");
-            String[] objectName = new String[actions.Count];
-            int[] objectType = new int[actions.Count];
-            if (pageName != null)
-            {
-                if (i != 0)
-                {
-                    objectType[i] = 7;
-                    i++;
+            XmlNodeList mainlist = v.GetElementsByTagName("Category");
+            for (int i = 0; i < mainlist.Count; i++) {
+                XmlNode category = mainlist[i];
+                if (pageName != category.Attributes["Page"].Value) {
+                    objectName.Add(category.Attributes["Page"].Value);
+                    objectType.Add(1);
+                    pageName = category.Attributes["Page"].Value;
                 }
-                objectName[i] = pageName;
-                i++;
+                objectName.Add(category.Attributes["Name"].Value);
+                objectType.Add(2);
+                XmlElement actions = category["Actions"];
+                loadObjects(actions);
             }
-            objectName[i] = catagoryName;
-            objectType[i] = 2;
-            for (i++; i < actions.Count;)
+
+            serv = new Net.NetworkServer(11111);
+            serv.Connected += Serv_Connected1;
+            serv.Disconnected += Serv_Disconnected;
+            serv.DataAvailable += Serv_DataAvailable1;
+            serv.Start(11111);
+
+            objectsText.Text = objectType.Count.ToString() + " Objects Included";
+            devicesText.Text = devices + " Devices Connected";
+            
+        }
+
+        private void Serv_DataAvailable1(object sender)
+        {
+            
+        } 
+
+        private void Serv_Disconnected(object sender)
+        {
+            
+        }
+
+        private void Serv_Connected1(object sender)
+        {
+             TcpClient client = (TcpClient)sender;
+            for(int i = 0; i < objectType.Count; i++)
             {
-                objectName[i] = actions[i].Attributes["Name"].ToString();
-                switch (actions[i].Attributes["Type"].ToString())
+                serv.SendPacket(objectName[i], objectType[i].ToString(), client);
+            }
+            devices++;
+            devicesText.Text = devices + " Devices Connected";
+        }
+
+        public void loadObjects(XmlElement category)
+        {
+            XmlNodeList actions = category.GetElementsByTagName("Action");
+            for (int i = 0; i < actions.Count;i++)
+            {
+                objectName.Add(actions[i].Attributes["Name"].Value);
+                switch (actions[i].Attributes["Type"].Value)
                 {
                     case "Check":
-                        objectType[i] = 3;
+                        objectType.Add(3);
                         break;
                     case "Counter":
-                        objectType[i] = 4;
+                        objectType.Add(4);
                         break;
                     case "Slider":
-                        objectType[i] = 5;
+                        objectType.Add(5);
                         break;
                     case "Choice":
-                        objectType[i] = 6;
-                        break;
-                    case "Text":
-                        objectType[i] = 8;
+                        objectType.Add(6);
                         break;
                 }
             }
