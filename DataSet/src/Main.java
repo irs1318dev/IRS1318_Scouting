88220b006fa2265c5f137f9e1433beec951af2f4
magital15 <talimagidson@hotmail.com>
2016-03-12 14:44:36 -0600

    set up basic output

--- DataSet/src/Main.java
+++ DataSet/src/Main.java
@@ -1,9 +1,18 @@
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		File file = new File("Matches.csv");
+		try {
+			file.createNewFile();
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write("Match,Team,Side,\n");
+			fileWriter.close();
+		}catch (IOException e) {}
 		MainData mainData = new MainData();
 		mainData.run(true);
 	}
--- DataSet/src/MainData.java
+++ DataSet/src/MainData.java
@@ -1,15 +1,19 @@
 import Net.*;
 
-import java.io.Console;
-import java.io.SyncFailedException;
+import java.io.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainData
 {
     int i = 0;
+    int j = 0;
+    int inMatch = 0;
     int[] objectType;
     String text;
     String[] objectName;
+    List<Integer> teams;
+    List<List<Integer[]>> dataValue;
     boolean connected = false;
     TCPClient client;
 
@@ -22,7 +26,9 @@ public class MainData
             System.out.println("Enter Server Address:");
             Scanner scanner = new Scanner(System.in);
             text = scanner.nextLine();
-            connect();
+            try {
+                connect();
+            }catch (Exception e) {}
         }
     }
 
@@ -62,26 +68,54 @@ public class MainData
 							objectName[i] = networkPackets[i].Data.split(",")[0];
 							text = networkPackets[i].Data.split(",")[1];
 							objectType[i] = Integer.valueOf(text);
-							saveData();
 						}
+                        if (networkPackets[i].Name.equals("MatchData"))
+                        {
+                            int team = Integer.valueOf(networkPackets[i].Data.split("&")[0].split(",")[0]);
+                            if(!teams.contains(team)) teams.add(team);
+                            inMatch++;
+                            if(inMatch > 6) inMatch = 1;
+                            if(inMatch > 3) text = "Blue";
+                            else text = "Red";
+                            try {
+                                File file = new File("Matches.csv");
+                                FileWriter fileWriter = new FileWriter(file);
+                                fileWriter.write(networkPackets[i].Data.split("&")[0].split(",")[1] + "," + team + "," + text + "\n");
+                                fileWriter.close();
+                            }catch (IOException e) {}
+
+                            String[] data = networkPackets[i].Data.split("&")[1].split(",");
+                            Integer[] values = new Integer[data.length];
+                            for(j = 0; j < data.length; j++) values[Integer.valueOf(data[j].split(":")[0])] = Integer.valueOf(data[j].split(":")[1]);
+
+                            dataValue.get(teams.indexOf(team)).add(values);
+                        }
 					}
 				}
 			});
-        try
-		{
+        try {
             client.Connect();
-        }
-		catch (Exception e)
-		{
-        }
+        }catch (Exception e) {}
         run(false);
     }
 
-    public void saveData()
+	public void saveData()
 	{
-        for (i = 0; i < objectName.length; i++)
-		{
-            System.out.print(objectName[i]);
-        }
-    }
+        try {
+            File file = new File("Data.csv");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("team,");
+
+            for (i = 0; i < objectName.length; i++) fileWriter.write(objectName[i] + ",");
+
+            for(i = 0; i < teams.size(); i++) {
+                fileWriter.write("\n");
+                fileWriter.write(teams.get(i) + ",");
+                List<Integer[]> matchList = dataValue.get(i);
+                for(j = 0; j < objectName.length; j++) fileWriter.write((matchList.get(matchList.size() - 1)[j] + matchList.get(matchList.size() - 2)[j] + matchList.get(matchList.size() - 3)[j]) + ",");
+            }
+            fileWriter.close();
+        }catch (IOException e) {}
+	}
 }
