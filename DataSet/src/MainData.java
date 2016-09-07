df5cc41e83cae39beeaf65c4d2d73e58ccae42f2
magital15 <talimagidson@hotmail.com>
2016-09-06 19:56:05 -0700

    checked dataview

--- DataSet/src/MainData.java
+++ DataSet/src/MainData.java
@@ -54,7 +54,6 @@ public class MainData {
 					NetworkPacket[] networkPackets = client.GetPackets();
                     int object = 0;
 					for (NetworkPacket networkPacket : networkPackets) {
-                        //System.out.println(networkPacket);
                         if(networkPacket.Name.equals("GameStart")) {
 							objectName = new String[networkPacket.DataAsInt()];
 							objectType = new int[networkPacket.DataAsInt()];
@@ -67,8 +66,7 @@ public class MainData {
                                     text = "(" + networkPacket.Data.split(",")[0].charAt(0) + ")";
                                     break;
                                 case 3:case 4:case 5:case 9:
-                                    String data = text + networkPacket.Data.split(",")[0];
-                                    objectName[object] = data;
+                                    objectName[object] = text + networkPacket.Data.split(",")[0];
 									objectType[object] = Integer.valueOf(networkPacket.Data.split(",")[1]);
 									break;
                             }
@@ -80,12 +78,13 @@ public class MainData {
                                 client.SendPacket("GetData", " ");
                             } catch (IOException e) {
                             }
+
+                            //Set columns
                             columnNames.add("Entered");
                             for(i = 0; i < objectName.length; i++)
                                 if (objectName[i] != null) {
                                     String name = objectName[i];
-                                    if(name.contains("$")) name = name.split("$")[0];
-                                    else if(!columnNames.contains(name)) columnNames.add(name);
+                                    columnNames.add(name);
                                 }
                         }
 						if(networkPacket.Name.equals("MatchEnd")) {
@@ -108,17 +107,18 @@ public class MainData {
             Scanner scanner = new Scanner(file);
             System.out.println("Found");
             position = 0;
+
             while(scanner.hasNextLine()) {
                 String[] line = scanner.nextLine().split("&");
                 if(position == 0) {
                     match = Integer.valueOf(line[0].split(",")[0]);
-                    System.out.println("Loading Match " + match);
                     position++;
                 } else {
                     int team = Integer.valueOf(line[0].split(",")[1]);
+
+                    //Set new position
                     if (position > 3) text = "Blue " + (position - 3);
                     else text = "Red " + position;
-                    int j = -1;
                     if(position > 3) i = 4;
                     position++;
                     if(position > 6) position = 0;
@@ -151,10 +151,12 @@ public class MainData {
         try {
             File file = new File("MatchData.csv");
             FileWriter dataWriter = new FileWriter(file);
+            //Write column names
             dataWriter.write("Match,Team,Alliance,");
             for (String columnName : columnNames) {
                 dataWriter.write(columnName + ",");
             }
+            //write actual data
             for(i = 0; i < matches.size(); i++) {
                 Integer[] matchList = dataValue.get(i);
                 dataWriter.write("\n" + matches.get(i));
